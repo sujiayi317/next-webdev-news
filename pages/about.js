fb4180c972456948dc9111603dc94523c5436c84
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-07-25 23:32:21 -0400

    Finish basic functionalities

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>About WebDev News</title>
+        <meta name='keywords' content='web development' />
+      </Head>
+      <h1>About</h1>
+    </div>
+  )
+}
+
+export default about
